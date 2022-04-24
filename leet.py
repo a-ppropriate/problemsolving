baff32dbb66e816ceb74a355f18648322bd9b932
a-ppropriate <sraniinik@gmail.com>
2022-04-24 19:29:45 +0700

    update CLI script

--- leet.py
+++ leet.py
@@ -1,15 +1,11 @@
+import sys
+import pyperclip
 import argparse
 import os
-from tkinter import Tk 
-	#from: https://stackoverflow.com/questions/101128/how-do-i-read-text-from-the-clipboard
-	#works for me
-	#TODO(?): investigate compatibility
+import json
 
-BASE_PATH ='./problems/leetcode/' #TODO(?): change to env var or argument later if the need arises
-
-if (os.path.exists(BASE_PATH) != True):
-	print ('Path "' + BASE_PATH + '" is not found, have a nice day')
-	exit(1)
+BASE_PATH ='./problems/leetcode/' 
+PROBLEM_FILENAME = 'problem.json' #TODO(?): move constants to a separate file to be used by both this and js script
 
 
 def yes_or_no(question, default = 'yes'):
@@ -43,7 +39,8 @@ help_descriptions = {
 	
 	'add': {
 		'_cmd_': 'Add problem',
-		'description': 'Problem description'
+		'description': 'Problem description',
+		'title': 'Problem title'
 	},
 	'comment': {
 		'_cmd_': 'Write/rewrite comment for the existing problem or the problem solution',
@@ -61,31 +58,125 @@ help_descriptions = {
 	
 }
 
-parser = argparse.ArgumentParser(description = 'something something')
-
-subparsers = parser.add_subparsers(help='sub-command help')
-parser_create = subparsers.add_parser('add', help=help_descriptions['add']['_cmd_'])
-parser_create.add_argument('pid', help = help_descriptions['pid'])
-parser_create.add_argument('description', help = help_descriptions['add']['description'])
-
-parser_delete = subparsers.add_parser('delete', help=help_descriptions['delete']['_cmd_'])
-parser_delete.add_argument('pid', help = help_descriptions['pid'])
-
-parser_comment = subparsers.add_parser('comment', help=help_descriptions['comment']['_cmd_'])
-parser_comment.add_argument('pid', help = help_descriptions['pid'])
-parser_comment.add_argument('lang', choices=['global','js','py'],help = help_descriptions['comment']['lang'],type=ascii)
-parser_comment.add_argument('text', help = help_descriptions['comment']['text'],type=ascii)
-
-parser_description = subparsers.add_parser('description', help=help_descriptions['description'])
-parser_description.add_argument('pid', help = help_descriptions['pid'])
-
-parser_reindex = subparsers.add_parser('reindex', help=help_descriptions['reindex']['_cmd_'])
 
 
-
-def add_problem (problem_id, description = None):
-	choice = yes_or_no ('Are you sure?','no');
-	return True
+def problem_add (pid = None, title = None):
+	"""
+		Creates a problem folder & .json file, containing problem data in it by using either arguments or JSON from clipboard
+	"""
+	clipboard = pyperclip.paste()
 	
-args = parser.parse_args()
-print (args)
+	data = None
+	description = ''
+	difficulty = ''
+	
+	try:
+		data = json.loads(clipboard)
+	except:
+		pass
+	
+	if (data != None and yes_or_no('Clipboard seems to contain a valid JSON, use it for problem creation?','yes')):
+		if (pid == None or (pid != data['id'] and yes_or_no('JSON contains an id different from the provided one, use id from JSON?','yes'))):
+			pid = data['id']
+
+		title = data['title']
+		description = data['description']
+		difficulty = data['difficulty']
+	else:
+		if (pid == None):
+			sys.exit('No id given')
+		if (title == None):
+			sys.exit('No title given')
+	
+	output_dict = {
+		'id': pid,
+		'title':title,
+		'description':description,
+		'difficulty':difficulty
+	};
+	
+	path_to_problem = BASE_PATH + pid + '/'
+	path_to_file = path_to_problem + PROBLEM_FILENAME
+	
+	if (os.path.exists(path_to_problem)):
+		if (yes_or_no('The problem folder is already in place, rewrite problem data?','no')):
+			pass
+		else:
+			sys.exit('You chose to not rewrite the problem data')
+	else:
+		try:
+			os.makedirs(path_to_problem)
+		except:
+			sys.exit('Unable to create folder')
+
+
+	try:
+		file = open(path_to_file,'w');
+		json.dump(output_dict,file);
+		file.close();
+	except:
+		sys.exit('Unable to create/write file')
+		
+	print('Problem created');
+	
+
+def problem_delete ():
+	pass
+
+def problem_comment ():
+	pass
+	
+command_mapping = { #TODO(?):find a prettier way to do it
+	'add':problem_add,
+	'delete':problem_delete,
+	'comment':problem_comment,
+}
+
+def main():
+	if (not os.path.exists(BASE_PATH)):
+		print ('Path "' + BASE_PATH + '" is not found, have a nice day')
+		exit(1)
+		
+	parser = argparse.ArgumentParser(description = 'A script for managing problems from leetcode')
+	command = None
+
+	subparsers = parser.add_subparsers(help='sub-command help',dest='command')
+	parser_create = subparsers.add_parser('add', help=help_descriptions['add']['_cmd_'])
+	parser_create.add_argument('pid', help = help_descriptions['pid'],nargs='?')
+	parser_create.add_argument('title', help = help_descriptions['add']['title'],nargs='?')
+
+	parser_delete = subparsers.add_parser('delete', help=help_descriptions['delete']['_cmd_'])
+	parser_delete.add_argument('pid', help = help_descriptions['pid'])
+
+	parser_comment = subparsers.add_parser('comment', help=help_descriptions['comment']['_cmd_'])
+	parser_comment.add_argument('pid', help = help_descriptions['pid'])
+	parser_comment.add_argument('lang', choices=['global','js','py'],help = help_descriptions['comment']['lang'],type=ascii)
+	parser_comment.add_argument('text', help = help_descriptions['comment']['text'],type=ascii)
+
+	parser_description = subparsers.add_parser('description', help=help_descriptions['description'])
+	parser_description.add_argument('pid', help = help_descriptions['pid'])
+
+	parser_reindex = subparsers.add_parser('reindex', help=help_descriptions['reindex']['_cmd_'])
+	
+	
+	args = vars(parser.parse_args())
+	command = args.pop('command');
+	
+	#print (args)
+	command_mapping[command](**args)
+	
+	#try:
+	#	command_mapping[command](args)
+	#except:
+	#	print ('Something went wrong')
+	#	exit(1);
+	
+	#print (command)
+	
+
+#try:
+#	command_mapping[args[0]]()
+#	break;
+
+if __name__ == "__main__":
+	main()
